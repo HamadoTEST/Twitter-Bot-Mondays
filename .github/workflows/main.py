1e560ac237e2b66346ec35af795a5d4e2b3352ff
HamadoTEST <121375604+HamadoTEST@users.noreply.github.com>
2024-08-06 15:17:55 +0300

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,38 @@
+import tweepy
+import requests
+import os
+import schedule
+import time
+
+# Your Twitter API credentials
+API_KEY = 'jJQ6srNAgh3dNFUutByA3d7zL'
+API_KEY_SECRET = 'SbcY3oqsxFWPNb69xdJuT7hfXe60Mi7uZdlhP0OB4719RXNYhZ'
+ACCESS_TOKEN = '1810007865354256384-XekdvB1egBQrpoGoAA5TEQSb18J23J'
+ACCESS_TOKEN_SECRET = 'cJgRMc0Bwk4DHnYD9apBn2GQ4IQfXoxrwdxMycDa1j8G7'
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAANC%2FuwEAAAAAtLNoAQYKxTKcb3doL6hYyfxkLLs%3DrmiO09qq3HtYGSuOylOanGf1oSX5XRSTugSiYjgEbZXk6FNCKO'
+
+# Authenticate to the Twitter API
+client = tweepy.Client(bearer_token=BEARER_TOKEN, consumer_key=API_KEY, consumer_secret=API_KEY_SECRET, access_token=ACCESS_TOKEN, access_token_secret=ACCESS_TOKEN_SECRET)
+
+# Authenticate using Tweepy's OAuth1UserHandler
+auth = tweepy.OAuth1UserHandler(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+def upload_video(file_path):
+    # Initialize the upload
+    media = api.media_upload(filename=file_path, media_category='tweet_video')
+
+    # Post the tweet with the uploaded video
+    response = client.create_tweet(media_ids=[media.media_id], text='Monday Monkey Lives For The Weekend, Sir')
+    print("Tweeted:", response.data)
+
+def job():
+    video_path = 'mondays.mp4'  # Assuming the video file is named 'mondays.mp4'
+    upload_video(video_path)
+
+# Schedule the job to run every Monday at 10 PM
+schedule.every().monday.at("22:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # Wait a minute before checking the schedule again
